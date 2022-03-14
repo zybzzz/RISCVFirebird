90008cc78073aaf11da226f07aeba1ab1dc112b2
zybzzz <1421904898@qq.com>
2022-03-14 20:32:12 +0800

    feat:  冒险单元多路选择器

---
+++ Pipeline/rtl/fb_alu.v
@@ -0,0 +1,16 @@
+//////////////////////////////////////
+//  Author: YiBo Zhang
+//  Date: 2022-03-14 14:09:50
+//  LastEditTime: 2022-03-14 14:15:15
+//  LastEditors: YiBo Zhang
+//  Description: this is alu
+//  
+ /////////////////////////////////////
+module fb_alu (
+  input [1:0] alu_op,
+  input [`FB_32BITS-1:0] op1,
+  input [`FB_32BITS-1:0] op2,
+  output [`FB_32BITS-1:0] alu_res
+);
+  
+endmodule
---
+++ Pipeline/rtl/fb_fwdmux.v
@@ -0,0 +1,31 @@
+//////////////////////////////////////
+//  Author: YiBo Zhang
+//  Date: 2022-03-14 13:39:48
+//  LastEditTime: 2022-03-14 14:08:36
+//  LastEditors: YiBo Zhang
+//  Description: this is data mux use for forwarding
+//  2 - 4 multiplexer
+/////////////////////////////////////
+
+
+/////////////////////////////////////
+// *        selcode         comment
+// *          00          data come from register
+// *          10          data come from alu result
+// *          01          data come from memory or alu result long from before
+/////////////////////////////////////
+`include "./fb_defines.v"
+module fb_fwdmux (
+  input [1:0] sel,
+  input [`FB_32BITS-1:0] reg_data,
+  input [`FB_32BITS-1:0] forward_10_data,
+  input [`FB_32BITS-1:0] forward_01_data,
+  output [`FB_32BITS-1:0] d
+);
+  
+assign d = (sel == 2'b10) ? forward_10_data : 
+           (sel == 2'b01) ? forward_01_data :
+           reg_data;
+
+
+endmodule
