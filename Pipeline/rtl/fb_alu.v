5b536ee7c8f0bd7b275ead82390e52d346f4d4dc
zybzzz <1421904898@qq.com>
2022-03-14 23:27:22 +0800

    style:  增加alu TODO

--- Pipeline/rtl/fb_alu.v
+++ Pipeline/rtl/fb_alu.v
@@ -1,16 +1,20 @@
 //////////////////////////////////////
 //  Author: YiBo Zhang
 //  Date: 2022-03-14 14:09:50
-//  LastEditTime: 2022-03-14 14:15:15
+//  LastEditTime: 2022-03-14 23:25:00
 //  LastEditors: YiBo Zhang
 //  Description: this is alu
 //  
  /////////////////////////////////////
 module fb_alu (
-  input [1:0] alu_op,
+  input [10:0] alu_control,
   input [`FB_32BITS-1:0] op1,
   input [`FB_32BITS-1:0] op2,
   output [`FB_32BITS-1:0] alu_res
+  // TODO some output csr signal
 );
+
+// TODO operate by alu control signal
+// TODO when branch == 1 set csr
   
 endmodule
