4c5d91438027023a9334b9f485fd2dd183f85da7
zybzzz <1421904898@qq.com>
2022-03-14 13:20:27 +0800

    feat:  EX/MEM MEM/WE寄存器子组件
    fix: 修改EX/MEM MEM/WB寄存器bug

--- Pipeline/rtl/fb_exmemreg.v
+++ Pipeline/rtl/fb_exmemreg.v
@@ -1,10 +1,15 @@
 //////////////////////////////////////
 //  Author: YiBo Zhang
 //  Date: 2022-03-14 10:47:18
-//  LastEditTime: 2022-03-14 11:03:05
+//  LastEditTime: 2022-03-14 12:07:06
 //  LastEditors: YiBo Zhang
 //  Description: this is EX/MEM register
 //  
+/////////////////////////////////////
+/////////////////////////////////////
+// Digital ports:
+// input: clk,we,rst,ex_mem_read,ex_mem_write,ex_branch,ex_mem_to_reg,ex_reg_write,ex_alu_res:32,ex_rs2_data:32,ex_register_rd:5
+// output: mem_mem_read,mem_mem_write,mem_branch,mem_mem_to_reg,mem_reg_write,mem_alu_res:32,mem_rs2_data:32,mem_register_rd:5
 /////////////////////////////////////
 `include "./fb_defines.v"
 module fb_exmemreg (
@@ -23,7 +28,7 @@ module fb_exmemreg (
   input [`FB_32BITS-1:0] ex_alu_res,    //alu calculate result
   input [`FB_32BITS-1:0] ex_rs2_data,   //rs2 data use for store
   // * register num
-  input [4:0] id_register_rd,           //register rd num use for data hazard unit
+  input [4:0] ex_register_rd,           //register rd num use for data hazard unit
   //output
   output reg mem_mem_read,
   output reg mem_mem_write,
