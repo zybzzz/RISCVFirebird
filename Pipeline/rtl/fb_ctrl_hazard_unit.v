633b18a749a8f2adf784414e43bff399df79cca8
zybzzz <1421904898@qq.com>
2022-03-15 12:22:57 +0800

    perf:  控制冒险单元
    完成对预测结果的判断

--- Pipeline/rtl/fb_ctrl_hazard_unit.v
+++ Pipeline/rtl/fb_ctrl_hazard_unit.v
@@ -1,34 +1,50 @@
 //////////////////////////////////////
 //  Author: YiBo Zhang
 //  Date: 2022-03-13 20:13:41
-//  LastEditTime: 2022-03-13 23:03:13
+//  LastEditTime: 2022-03-15 12:20:44
 //  LastEditors: YiBo Zhang
 //  Description: this is control hazard unit 
-//  ! now can't find method to judge predict error
+//  ! continuous branch question
 /////////////////////////////////////
 `include "./fb_defines.v"
 module fb_ctrl_hazard_unit (
   input [`FB_32BITS-1:0] pc,        //pc
   input [`FB_32BITS-1:0] inst,      //instruction
   input [`FB_32BITS-1:0] imm,       //offset
-  input branch,                     //is b-type instruction
-  input [`FB_32BITS-1:0] alu_result,                 //alu result, decide whether b-type instruction success
+  input branch,                     //is b-type instruction(after exe phase, from ex/mem register)
+  input NF,
+  input ZF,
+  input CF,
+  input VF,                         //csr status
   output address_src,                    //instruction address 0:predict_pc(success) 1:predict_err_pc(predict error)
   output predict_pc,                //pc generate by static prediction
   output predict_err_pc,            //pc when prediction error
   output register_rst               //reset the IF/ID ID/EX EX/MEM when prediction error
 );
 
-wire j_type = (pc[6:0] == 7'b1101111);
-wire jalr_type = (pc[6:0] == 7'b 1100111);
-wire b_type = (pc[6:0] == 7'b1100011);
 
-wire beq_ins = (inst[14:12] == 3'b000);
-wire bne_ins = (inst[14:12] == 3'b001);
-wire blt_ins = (inst[14:12] == 3'b100);
-wire bge_ins = (inst[14:12] == 3'b101);
-wire bltu_ins = (inst[14:12] == 3'b110);
-wire bgeu_ins = (inst[14:12] == 3'b111);
+wire j_type;
+wire jalr_type;
+wire b_type;
+
+wire beq_ins;
+wire bne_ins;
+wire blt_ins;
+wire bge_ins;
+wire bltu_ins;
+wire bgeu_ins;
+
+
+assign j_type = (pc[6:0] == 7'b1101111);
+assign jalr_type = (pc[6:0] == 7'b 1100111);
+assign b_type = (pc[6:0] == 7'b1100011);
+
+assign beq_ins = (inst[14:12] == 3'b000);
+assign bne_ins = (inst[14:12] == 3'b001);
+assign blt_ins = (inst[14:12] == 3'b100);
+assign bge_ins = (inst[14:12] == 3'b101);
+assign bltu_ins = (inst[14:12] == 3'b110);
+assign bgeu_ins = (inst[14:12] == 3'b111);
 
 //offset of jump (signal extent by imm generate unit)
 wire [`FB_32BITS-1:0] offset;
@@ -44,12 +60,14 @@ assign predict_err_pc = (offset[`FB_32BITS-1] == 1)? pc + 1 :   //b-type jump ba
                         pc + offset;                            //b-type jump front
 
 wire is_predict_error;
-//TODO how to judge predict_error
-assign is_predict_error = (branch == 0) ? 0 :
-                          (beq_ins == 1) ? (alu_result != 32'b0) :
-                          (bne_ins == 1) ? (alu_result == 32'b0) :
-                          (blt_ins == 1) ? 1 :
-                          0;
+
+//* use csr status and branch type to judge predict error
+assign is_predict_error = ((branch & b_type & beq_ins) & (ZF == 1'b0))
+                        | ((branch & b_type & bne_ins) & (ZF == 1'b1))
+                        | ((branch & b_type & blt_ins) & (NF == VF))
+                        | ((branch & b_type & bge_ins) & (NF != VF))
+                        | ((branch & b_type & bltu_ins) & (CF == 1'b1))
+                        | ((branch & b_type & bgeu_ins) & (CF == 1'b0));
 //change pc_scr and register_rst status when predict error
 assign address_src = is_predict_error;
 assign register_rst = is_predict_error;
